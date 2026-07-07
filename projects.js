6c17e9ee293a1b124b8d0328fe90be68129ce5c9
occhinog <gabrielocchino@icloud.com>
2026-07-07 22:36:15 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,5 +1,8 @@
 const projects = [
-    { name: "Caffetteria", link: "caffetteria/" },
     { name: "Elezioni2018", link: "elezioni18/" },
     { name: "Elezioni2023", link: "elezioni23/" }
+];
+const subdomains = [
+    { name: "Progetti", link: "caffetteria.occhino.it" },
+    { name: "Caffetteria", link: "progetti.occhino.it" }
 ];
