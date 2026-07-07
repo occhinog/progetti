25dff0d7ee58a28613b27734147060ff6446e91b
occhinog <gabrielocchino@icloud.com>
2026-07-07 23:04:33 +0200

    Remove subdomains array from projects.js
    Removed subdomains array from projects.js.

--- projects.js
+++ projects.js
@@ -2,7 +2,3 @@ const projects = [
     { name: "Elezioni2018", link: "elezioni18/" },
     { name: "Elezioni2023", link: "elezioni23/" }
 ];
-const subdomains = [
-    { name: "Progetti", link: "caffetteria.occhino.it" },
-    { name: "Caffetteria", link: "progetti.occhino.it" }
-];
