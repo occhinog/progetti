37fde4f96c8807b8e0b980a99e5fae9b6c822bc0
Gabriel <300979470+occhinog@users.noreply.github.com>
2026-09-04 00:34:55 +0200

    Add Finance Reports to project hub

--- projects.js
+++ projects.js
@@ -1,5 +1,6 @@
 const projects = [
     { name: "Elezioni2018", link: "elezioni18/" },
     { name: "Elezioni2023", link: "elezioni23/" },
-    { name: "SMARTAI / BusinessAI Academy", link: "businessai/" }
+    { name: "SMARTAI / BusinessAI Academy", link: "businessai/" },
+    { name: "Finance Reports", link: "finance/" }
 ];
