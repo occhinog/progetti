251da963a61d5b5d5d4531d3dcaba93d950d2f41
Gabriel <300979470+occhinog@users.noreply.github.com>
2026-09-11 01:01:52 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,5 +2,6 @@ const projects = [
     { name: "Elezioni2018", link: "elezioni18/" },
     { name: "Elezioni2023", link: "elezioni23/" },
     { name: "SMARTAI / BusinessAI Academy", link: "businessai/" },
-    { name: "Finance Reports", link: "finance/" }
+    { name: "Finance Reports", link: "finance/" },
+    { name: "Carport Italia", link: "carportitalia/" }
 ];
